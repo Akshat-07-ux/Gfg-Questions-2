eea7ca8186d3cc1735225d8f3db3ea21f8acb167
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-10-04 19:26:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Expression Add Operators/expression-add-operators.cpp
+++ Difficulty: Hard/Expression Add Operators/expression-add-operators.cpp
@@ -3,40 +3,41 @@ using namespace std;
 
 class Solution {
   public:
+    vector<string> ans;
+
+    void backtrack(string &s, int target, int idx, long long currVal, long long prevNum, string expr) {
+        // Base case: if we reach end of string
+        if (idx == s.size()) {
+            if (currVal == target) ans.push_back(expr);
+            return;
+        }
+        
+        // Form numbers by picking digits one by one
+        for (int i = idx; i < s.size(); i++) {
+            // Avoid numbers with leading zero
+            if (i > idx && s[idx] == '0') break;
+
+            string part = s.substr(idx, i - idx + 1);
+            long long num = stoll(part);
+
+            if (idx == 0) {
+                // First number, start expression
+                backtrack(s, target, i + 1, num, num, part);
+            } else {
+                // '+' operator
+                backtrack(s, target, i + 1, currVal + num, num, expr + "+" + part);
+                // '-' operator
+                backtrack(s, target, i + 1, currVal - num, -num, expr + "-" + part);
+                // '*' operator (handle precedence)
+                backtrack(s, target, i + 1, currVal - prevNum + prevNum * num, prevNum * num, expr + "*" + part);
+            }
+        }
+    }
+
     vector<string> findExpr(string &s, int target) {
-        vector<string> res;
-        string expr;
-
-        function<void(int,long long,long long,string)> dfs = [&](int idx, long long eval, long long prev, string path) {
-            // If we've reached the end of the string
-            if (idx == s.size()) {
-                if (eval == target) {
-                    res.push_back(path);
-                }
-                return;
-            }
-
-            long long num = 0;
-            for (int i = idx; i < s.size(); i++) {
-                // prevent leading zero numbers
-                if (i > idx && s[idx] == '0') break;
-
-                num = num * 10 + (s[i] - '0');
-                string cur = s.substr(idx, i - idx + 1);
-
-                if (idx == 0) {
-                    // first number, no operator before it
-                    dfs(i + 1, num, num, cur);
-                } else {
-                    dfs(i + 1, eval + num, num, path + "+" + cur);
-                    dfs(i + 1, eval - num, -num, path + "-" + cur);
-                    dfs(i + 1, eval - prev + prev * num, prev * num, path + "*" + cur);
-                }
-            }
-        };
-
-        dfs(0, 0, 0, "");
-        sort(res.begin(), res.end()); // lexicographical order
-        return res;
+        ans.clear();
+        backtrack(s, target, 0, 0, 0, "");
+        sort(ans.begin(), ans.end()); // Lexicographically smallest order
+        return ans;
     }
 };
