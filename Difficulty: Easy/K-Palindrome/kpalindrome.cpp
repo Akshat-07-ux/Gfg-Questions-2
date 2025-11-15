de21ba2c3b5e888c054a93edb93541bc9c69c42d
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2025-11-15 20:24:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-Palindrome/kpalindrome.cpp
@@ -0,0 +1,22 @@
+bool iskPalindrome(string s, int k) {
+    int n = s.size();
+    string t = s;
+    reverse(t.begin(), t.end());
+
+    // LCS to compute LPS
+    vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s[i-1] == t[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    int lps = dp[n][n];
+    int minDel = n - lps;
+
+    return minDel <= k;
+}
