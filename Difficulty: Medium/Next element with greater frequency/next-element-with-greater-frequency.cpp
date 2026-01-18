5974dc0b5104e96eac11841dc79525b112fd08eb
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2026-01-18 18:32:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.cpp
@@ -1,38 +1,32 @@
-#include <vector>
-#include <unordered_map>
-#include <stack>
-using namespace std;
-
 class Solution {
   public:
-    vector<int> findGreater(vector<int>& arr) {
+    vector<int> nextFreqGreater(vector<int>& arr) {
         int n = arr.size();
-        vector<int> res(n, -1);
-
-        // Step 1: Calculate frequencies
+        vector<int> result(n, -1);
         unordered_map<int, int> freq;
-        for (int num : arr) {
-            freq[num]++;
+        stack<int> st;
+
+        // Step 1: Count frequencies
+        for (int x : arr) {
+            freq[x]++;
         }
 
-        // Step 2: Use stack to find next element with higher frequency
-        stack<int> st; // stack stores indices
-
-        for (int i = n - 1; i >= 0; --i) {
-            // Pop elements that have freq <= current
-            while (!st.empty() && freq[arr[st.top()]] <= freq[arr[i]]) {
+        // Step 2: Traverse from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            // Remove elements with frequency <= current
+            while (!st.empty() && freq[st.top()] <= freq[arr[i]]) {
                 st.pop();
             }
 
-            // If stack not empty, that means top is the required element
+            // If stack not empty, top is the answer
             if (!st.empty()) {
-                res[i] = arr[st.top()];
+                result[i] = st.top();
             }
 
-            // Push current index
-            st.push(i);
+            // Push current element
+            st.push(arr[i]);
         }
 
-        return res;
+        return result;
     }
 };
