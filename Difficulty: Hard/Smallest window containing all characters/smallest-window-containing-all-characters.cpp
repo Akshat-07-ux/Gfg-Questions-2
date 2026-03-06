b7f6008081a3c4756221bf7528d5b52d78ebb80b
Akshat Bhagat <146670427+Akshat-07-ux@users.noreply.github.com>
2026-03-06 19:26:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -1,52 +1,49 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
   public:
-    string smallestWindow(string &s, string &p) {
-        if (p.size() > s.size()) return "";
+    string minWindow(string &s, string &p) {
+        int n = s.length();
+        int m = p.length();
 
-        // Frequency arrays for lowercase letters
-        vector<int> need(26, 0), have(26, 0);
+        if (m > n) return "";
 
-        // Fill need[]
-        for (char c : p) need[c - 'a']++;
+        // Frequency maps for characters (using array for speed with lowercase English letters)
+        int p_map[26] = {0};
+        int s_map[26] = {0};
 
-        int required = 0; 
-        for (int x : need) if (x > 0) required++;
-
-        int formed = 0;
-        int left = 0, right = 0;
-        int minLen = INT_MAX, start = -1;
-
-        while (right < s.size()) {
-            char c = s[right];
-            have[c - 'a']++;
-
-            if (need[c - 'a'] > 0 && have[c - 'a'] == need[c - 'a'])
-                formed++;
-
-            // When all required chars are satisfied
-            while (left <= right && formed == required) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    start = left;
-                }
-
-                // Try to shrink window
-                char d = s[left];
-                have[d - 'a']--;
-                if (need[d - 'a'] > 0 && have[d - 'a'] < need[d - 'a'])
-                    formed--;
-
-                left++;
-            }
-            right++;
+        for (char c : p) {
+            p_map[c - 'a']++;
         }
 
-        if (start == -1) return "";
-        return s.substr(start, minLen);
+        int start = 0, min_len = INT_MAX, start_idx = -1;
+        int count = 0; // Number of characters in s that match p's requirements
+
+        for (int j = 0; j < n; j++) {
+            int char_idx = s[j] - 'a';
+            s_map[char_idx]++;
+
+            // If current char is needed by p and we haven't exceeded its required count
+            if (p_map[char_idx] != 0 && s_map[char_idx] <= p_map[char_idx]) {
+                count++;
+            }
+
+            // If the window contains all characters of p
+            if (count == m) {
+                // Try to shrink the window from the left
+                while (s_map[s[start] - 'a'] > p_map[s[start] - 'a'] || p_map[s[start] - 'a'] == 0) {
+                    s_map[s[start] - 'a']--;
+                    start++;
+                }
+
+                // Update minimum window
+                int window_len = j - start + 1;
+                if (window_len < min_len) {
+                    min_len = window_len;
+                    start_idx = start;
+                }
+            }
+        }
+
+        if (start_idx == -1) return "";
+        return s.substr(start_idx, min_len);
     }
 };
-
-
