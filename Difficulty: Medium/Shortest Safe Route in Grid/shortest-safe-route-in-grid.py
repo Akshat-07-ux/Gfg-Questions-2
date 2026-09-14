1a18e6bd51e65d35fae964c22369981c243f0f22
Akshat <146670427+Akshat-07-ux@users.noreply.github.com>
2026-09-14 11:34:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Safe Route in Grid/shortest-safe-route-in-grid.py
+++ Difficulty: Medium/Shortest Safe Route in Grid/shortest-safe-route-in-grid.py
@@ -5,9 +5,9 @@ class Solution:
         # code here
         a = len(mat)
         b = len(mat[0])
-        
+
         safe = [[True] * b for _ in range(a)]
-        
+
         for i in range(a):
             for j in range(b):
                 if mat[i][j] == 0:
@@ -16,27 +16,26 @@ class Solution:
                         ni, nj = i + rd, j + cd
                         if 0 <= ni < a and 0 <= nj < b:
                             safe[ni][nj] = False
-                            
+
         queue = deque()
         visit = [[False] * b for _ in range(a)]
-        
+
         for i in range(a):
             if safe[i][0]:
                 queue.append((i, 0, 1))
                 visit[i][0] = True
-                
+
         while queue:
             r,c, dis = queue.popleft()
-            
+
             if c == b - 1:
                 return dis
-                
-                
+
+
             for rd, cd in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nr, nc = r + rd, c + cd
                 if 0 <= nr < a and 0 <= nc < b and safe[nr][nc] and not visit[nr][nc]:
                     visit[nr][nc] = True
                     queue.append((nr, nc, dis + 1))
-                    
+
         return -1
-        
